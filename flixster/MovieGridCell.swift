737e68626a326f195a59939194ab95c29c99c28d
Duy Le <leduy10192@gmail.com>
2020-02-25 16:35:09 -0800

    Complete Unit 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Duy Le on 2/24/20.
+//  Copyright © 2020 Duy Le. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
